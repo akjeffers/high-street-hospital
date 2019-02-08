4cf1097bb029ea17d5cf54d306490ebd93ef69d6
Ariel Webb <arielwebb12@gmail.com>
2019-02-08 16:43:14 -0500

    janitor stuff

--- src/test/java/employees/JanitorTest.java
+++ src/test/java/employees/JanitorTest.java
@@ -8,25 +8,38 @@ import org.junit.Test;
 public class JanitorTest {
 
 	Janitor underTest;
-	
-
-
 	@Before
 	public void setup() {
-		underTest = new Janitor("");
-		
+		underTest = new Janitor("Jorge", "234", false);
 	}
 	
 	@Test
 	public void shouldHaveName() {
-		assertEquals(underTest.getName(), "");
+		assertEquals("Jorge", underTest.getName());
 	}
 
 	@Test 
 	public void shouldCleanHospital() {
-		Boolean hospitalIsDirty
+		int cleanlinessBeforeCleaning = underTest.getHospitalCleanliness();
 		underTest.cleanHospital();
-		
+		int cleanlinessAfterCleaning = underTest.getHospitalCleanliness();
+		assertEquals(cleanlinessAfterCleaning, cleanlinessBeforeCleaning + 10);
 	}
 	
+	@Test
+	public void shouldDoMainWorkTask() {
+		boolean mainTaskStatusBefore = underTest.getMainTaskStatus();
+		underTest.toggleWorkStatus();
+		boolean mainTaskStatusAfter = underTest.getMainTaskStatus();
+		assertEquals(true, mainTaskStatusAfter);
+	}
+	
+	@Test
+	public void shouldStopDoingMainWorkTask() {
+		boolean workStatusBeforeClockout = underTest.getMainTaskStatus();
+		underTest.toggleWorkStatus();
+		underTest.toggleWorkStatus();
+		boolean workStatusAfterClockout = underTest.getMainTaskStatus();
+		assertEquals(false, workStatusAfterClockout);
+	}
 }
