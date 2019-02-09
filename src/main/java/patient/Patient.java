eee53d24a5798b9e973cb46bf85c2be5087e2391
akjeffers <aliciakjeffers@live.com>
2019-02-09 18:36:49 -0500

    patient-class-test

--- src/main/java/patient/Patient.java
+++ src/main/java/patient/Patient.java
@@ -2,4 +2,26 @@ package patient;
 
 public class Patient {
 
+	private String name;
+	private int bloodLevel;
+	private int healthLevel;
+
+	public Patient(String name, int bloodLevel, int healthLevel) {
+		this.name = name;
+		this.bloodLevel = 50;
+		this.healthLevel = 50;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getBloodLevel() {
+		return bloodLevel;
+
+	}
+
+	public int getHealthLevel() {
+		return healthLevel;
+	}
 }
